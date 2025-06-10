8cc3e6d686c2f92e23bdc2c7968b45a6eec338dc
BraeMonk <160609439+BraeMonk@users.noreply.github.com>
2025-06-09 23:28:19 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,30 @@
+const CACHE_NAME = 'fish-tally-v2';
+
+const urlsToCache = [
+  '/Fishin/',
+  '/Fishin/index.html',
+  '/Fishin/manifest.json',
+  '/Fishin/styles.css',
+  '/Fishin/script.js',
+  '/Fishin/icon_192x192.webp',
+  '/Fishin/icon_512x512.webp'
+];
+
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('fish-tally').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './styles.css',
-        './script.js',
-        './icon_192x192.webp',
-        './icon_512x512.webp'
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
   );
 });
 
