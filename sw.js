66c5a71d170cb95a3839a0381759aae46dfa096e
BraeMonk <160609439+BraeMonk@users.noreply.github.com>
2025-06-12 13:15:39 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fish-tally-v10'; // Increment this when deploying new versions
+const CACHE_NAME = 'fish-tally-v11'; // Increment this when deploying new versions
 
 const urlsToCache = [
   './',
